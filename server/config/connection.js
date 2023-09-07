b7c8c3d630f13bd3f3a99d137b0066edc5206591
Leone Varando <lvarando@gmail.com>
2023-09-07 17:41:14 -0500

    Began creating skeleton file structure; Added server and client folders through express and react respectivley

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config();
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/messenger-app-db',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
