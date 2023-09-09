6cb2f685372f43cb21bc08b03a40d1d1c1c3ced2
Leone Varando <lvarando@gmail.com>
2023-09-09 17:14:43 -0500

    Added conversation to main app

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import NavBar from'./components/navBar/index';
 import ConversationList from './components/conversationList';
+import Conversation from './components/conversation';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ConversationList />
+      <Conversation />
     </div>
   );
 }
