940ea4894e8c6feb1203bcda3b5c7ad4a7c5c79d
Leone Varando <lvarando@gmail.com>
2023-09-20 19:09:42 -0500

    Modified user model and included hashing func for password

--- server/models/user.js
+++ server/models/user.js
@@ -2,16 +2,43 @@ const { Schema, default: mongoose } = require('mongoose')
 
 const userSchema = new Schema(
   {
-    firstName: { type: String, required: true },
+    firstName: { 
+      type: String, 
+      required: true 
+    },
     lastName: String,
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true }
+    userName: { 
+      type: String, 
+      unique: true, 
+      required: true 
+    },
+    email: { 
+      type: String, 
+      unique: true, 
+      required: true, 
+      match: [/.+@.+\..+/, 'Must match an email address!'] 
+    },
+    password: { 
+      type: String, 
+      required: true, 
+      minLength: 8 
+    }
   },
   {
     timestamps: true
   }
 )
+
+userSchema.pre('save', async function(next) {
+  if (this.isNew || this.isModified('password')) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds)
+  }
+})
+
+userSchema.methods.isCorrectPassword = async (password) => {
+  return bcrypt.compare(password, this.password);
+}
 
 const User = mongoose.model('User', userSchema)
 
