c3dd656b9586636b4ea60358e292e75962364aa1
Leone Varando <lvarando@gmail.com>
2023-10-01 05:06:33 -0500

    Added search bar to navbar for friend search; Added styling for search bar

--- client/src/components/navBar/index.js
+++ client/src/components/navBar/index.js
@@ -4,6 +4,7 @@ const NavBar = () => {
   return (
     <nav id='nav-bar'>
       <h1 id='page-title' className='nav-child'>App Name</h1>
+      <input className='search-bar' type='text' placeholder='Search for Friends'></input>
       <img alt='profile' src='' className='nav-child'></img>
     </nav>
   )
