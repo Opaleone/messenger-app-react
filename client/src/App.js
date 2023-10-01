529620c3868f750b81fce8ccf673f93727a89b18
Leone Varando <lvarando@gmail.com>
2023-10-01 02:50:07 -0500

    Added routes for page selection; Added signup page to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Main from './pages/main';
+import Signup from './pages/signup';
 
 function App() {
   return (
-    <div className="App">
-      <Main />
-    </div>
+    <Router>
+      <div className='App'>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/main' element={<Main />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
