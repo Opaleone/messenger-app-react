98e8601ced0725ac2de217f911b4732e44cdd8d6
Leone Varando <lvarando@gmail.com>
2023-10-01 05:22:45 -0500

    Refactored for better legibility; Added use state for friend search

--- client/src/components/navBar/index.js
+++ client/src/components/navBar/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NavBar = () => {
+export default function NavBar() {
+  const [friendSearch, setFriendSearch] = useState('');
+
   return (
     <nav id='nav-bar'>
-      <h1 id='page-title' className='nav-child'>App Name</h1>
-      <input className='search-bar' type='text' placeholder='Search for Friends'></input>
-      <img alt='profile' src='' className='nav-child'></img>
+      <h1 
+        id='page-title' 
+        className='nav-child'
+        >App Name</h1>
+      <input 
+        className='search-bar' 
+        type='text' 
+        placeholder='Search for Friends'
+        value={friendSearch}
+        onChange={e => setFriendSearch(e.target.value)}
+        />
+      <img
+        alt='profile' 
+        src='' 
+        className='nav-child'></img>
     </nav>
   )
 }
-
-export default NavBar;
