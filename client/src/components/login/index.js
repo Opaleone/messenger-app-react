fbc0647917a3fabdf163e14144b79b955bad15b3
Leone Varando <lvarando@gmail.com>
2023-10-02 21:17:52 -0500

    Abstracted login into a component to be mounted in home page

---
+++ client/src/components/login/index.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from 'axios';
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleClick() {
+
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log(username);
+    console.log(password);
+
+    await axios.post('//localhost:3001/api/users', {
+      username: username,
+      password: password
+    })
+  }
+
+  return (
+    <>
+      <form 
+        className="form-container"
+        onSubmit={handleSubmit}
+        >
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+        />
+        <input
+          className="form-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button
+          className="form-button"
+          type="submit">Login</button>
+      </form>
+    </>
+  )
+}
