f173a7b3c56dbb77b790482b7e309a1b1af77d60
Leone Varando <lvarando@gmail.com>
2023-10-02 21:22:33 -0500

    Fixed file to reflect changes in files

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home'
 import Main from './pages/main';
-import Signup from './pages/signup';
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <Routes>
-          <Route path='/signup' element={<Signup />} />
+          <Route path='/' element={<Home />} />
           <Route path='/main' element={<Main />} />
         </Routes>
       </div>
