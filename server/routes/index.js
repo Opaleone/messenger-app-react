a0f6048af4e3480fdc2cfb76c910c06b2d21db13
Leone Varando <lvarando@gmail.com>
2023-10-04 17:38:08 -0500

    Added minor change for better dev experience

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const apiRoutes = require('./api')
 
+const apiRoutingChoice = ['/users', '/users/:id', '/users/:id/friends/:friendId' , '/messages', '/messages/:id/conversation/:friendId', '/messages/:id', '/messages/:id/singleMessage/:friendId']
+
 router.use('/api', apiRoutes);
 
 router.use((req, res) => {
-  return res.send('Wrong Route!')
+  const randomIdx = Math.floor(Math.random() * apiRoutingChoice.length)
+
+  return res.send(`Try localhost:3001/api${apiRoutingChoice[randomIdx]}`)
 })
 
 module.exports = router;
